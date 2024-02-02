2495aa5c016f71c0deba823fb9aee7859f9a7f39
Md. Abdul Aziz <abdulazizapu@gmail.com>
2024-02-03 00:05:23 +0600

    fix the appReducer type issue

--- lib/reducer.ts
+++ lib/reducer.ts
@@ -25,7 +25,7 @@ const initialState = {
   }
 
   // Use the initialState as a default value
-  export default function appReducer(state = initialState, action: PayloadAction<number>) {
+  export default function appReducer(state = initialState, action) {
   // The reducer normally looks at the action type field to decide what happens
   switch (action.type) {
     // Do something here based on the different types of actions
@@ -44,7 +44,6 @@ const initialState = {
             id: nextTodoId(state.todos),
             text: action.payload,
             completed: false,
-            color: 'green'
           }
         ]
       }
